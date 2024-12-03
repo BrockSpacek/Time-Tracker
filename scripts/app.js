7ab162f4a5e82118a6e48450906d1ba221a3a676
BrockSpacek <brockspacek1@gmail.com>
2024-12-03 14:21:29 -0800

    Added element ID's and empty button functions for later use

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+// Elements for Buttons
+let dailyBtn = document.getElementById('dailyBtn');
+let weeklyBtn = document.getElementById('weeklyBtn');
+let monthlyBtn = document.getElementById('monthlyBtn');
+
+// Elements for Data 
+let workCurrentHours = document.getElementById('workCurrentHours');
+let workPreviousHours = document.getElementById('workPreviousHours');
+let playCurrentHours = document.getElementById('playCurrentHours');
+let playPreviousHours = document.getElementById('playPreviousHours');
+let studyCurrentHours = document.getElementById('studyCurrentHours');
+let studyPreviousHours = document.getElementById('studyPreviousHours');
+let exerciseCurrentHours = document.getElementById('exerciseCurrentHours');
+let exercisePreviousHours = document.getElementById('exercisePreviousHours');
+let socialCurrentHours = document.getElementById('socialCurrentHours');
+let socialPreviousHours = document.getElementById('socialPreviousHours');
+let selfCareCurrentHours = document.getElementById('selfCareCurrentHours');
+let selfCarePreviousHours = document.getElementById('selfCarePreviousHours');
+
+
+// Functions
+
+dailyBtn.addEventListener('click', function(){
+
+});
+
+
+
+weeklyBtn.addEventListener('click', function(){
+
+});
+
+
+
+monthlyBtn.addEventListener('click', function(){
+
+});
